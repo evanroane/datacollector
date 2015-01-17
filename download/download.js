4153f6f0c08df99edd8b8977cc7493372e04b7d9
evanroane <evan.roane@gmail.com>
2015-01-17 13:25:20 -0600

    set up for configuring download

---
+++ download/download.js
@@ -0,0 +1,29 @@
+var sessionData = {
+  "behaviorInstances":[
+    {"name":"aggression","time":3,"x":0},
+    {"name":"i_mand","time":4,"x":1},
+    {"name":"prompt","time":5,"x":2},
+    {"name":"aggression","time":7,"x":3},
+    {"name":"i_mand","time":7,"x":4},
+    {"name":"p_mand","time":9,"x":5},
+    {"name":"p_mand","time":9,"x":6},
+    {"name":"p_mand","time":9,"x":7},
+    {"name":"p_mand","time":9,"x":8},
+    {"name":"p_mand","time":9,"x":9},
+    {"name":"p_mand","time":10,"x":10},
+    {"name":"p_mand","time":10,"x":11}
+  ],
+"codeSetName":"FCT",
+"description":"a",
+"endDate":1421015323220,
+"name":"with round",
+"startDate":1421015311690,
+"summary":[
+    {"frequency":2,"name":"aggression","rpm":0.01},
+    {"frequency":2,"name":"i_mand","rpm":0.01},
+    {"frequency":7,"name":"p_mand","rpm":0.04},
+    {"frequency":1,"name":"prompt","rpm":0.01}
+  ]
+}
+
+// FUNCTIONS:
